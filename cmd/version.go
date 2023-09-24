7956f236e6860cb4d639dc4f80a8d570fee8c929
Pandaft <Allovesx@gmail.com>
2023-09-24 14:20:35 +0800

    ✨ feat: 新增输出版本命令

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+const version = "v0.0.1"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "输出版本",
+	Long:  "输出当前 go-esurfing 具体版本",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
