c4fccd29dab62003ad68fbf573cbe5db01667321
Pandaft <Allovesx@gmail.com>
2023-10-09 11:40:17 +0800

    💦 chore: 根命令帮助描述新增显示版本

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ const (
 var rootCmd = &cobra.Command{
 	Use:   "go-esurfing",
 	Short: shortDesc,
-	Long:  fmt.Sprintf("%s\nGitHub: %s", shortDesc, githubUrl),
+	Long:  fmt.Sprintf("%s (%s)\nGitHub: %s", shortDesc, version, githubUrl),
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Warn("此程序为命令行工具，请带命令和参数运行，程序将在 3 秒后自动关闭...")
 		time.Sleep(time.Second * 3)
